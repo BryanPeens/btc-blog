025aa96ef1539e6822a6d599a6c9604e898fe9a4
Bryan Peens <peensbryan1@gmail.com>
2024-02-09 23:50:51 -0500

    update page text size

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -15,7 +15,7 @@ export default async function Project({ params }: Props) {
     return (
         <div>
             <header className="flex items-center justify-between">
-                <h1 className="bg-gradient-to-r from-slate-500 via-green-300 to-blue-600 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold py-2">{project.name}</h1>
+                <h1 className="bg-gradient-to-r from-slate-500 via-green-300 to-blue-600 bg-clip-text text-transparent md:text-3xl sm:text-3xl lg:text-4xl drop-shadow font-extrabold py-2">{project.name}</h1>
             </header>
 
             {/* content goes here */}
