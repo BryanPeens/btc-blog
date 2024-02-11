16a3c1005a39992dfbb173545111fab50f44f78f
Bryan Peens <peensbryan1@gmail.com>
2024-02-11 03:04:41 -0500

    install vercel analytics

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -6,6 +6,8 @@ import Header from "./Navbar";
 import Footer from "./Footer";
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 import "../globals.css";
+import { Analytics } from '@vercel/analytics/react';
+
 
 interface Page {
   _id: string;
@@ -29,19 +31,17 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     <html lang="en">
       <head>
         <title>Bridging The Canyon</title>
+        <meta name="p:domain_verify" content="18b2e53885de597dcf553b1c4463befc"/>
 
-        <meta
-          name="p:domain_verify"
-          content="18b2e53885de597dcf553b1c4463befc"
-        />
       </head>
       <body className="mx-auto bg-center">
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || ''} />
-        <GoogleTagManager gtmId={"GTM-K7JT9LJX"  || ''}  />
+        <GoogleTagManager gtmId={"GTM-K7JT9LJX"}  />
 
         <Header title="Bridging The Canyon" pages={pages} />
 
         <main className="p-2 mt-20">{children}</main>
+        <Analytics />
 
         <Footer/>
 
