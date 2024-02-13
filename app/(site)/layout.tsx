ae00483776c213f3022b2b6b783a042011e4e55a
Bryan Peens <peensbryan1@gmail.com>
2024-02-13 03:04:29 -0500

    added new package

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -7,6 +7,7 @@ import Footer from "./Footer";
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 import "../globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 interface Page {
@@ -32,17 +33,15 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <head>
         <title>Bridging The Canyon</title>
         <meta name="p:domain_verify" content="18b2e53885de597dcf553b1c4463befc"/>
-
-      </head>
-      <body className="mx-auto bg-center">
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || ''} />
         <GoogleTagManager gtmId={"GTM-K7JT9LJX"}  />
-
+      </head>
+      <body className="mx-auto bg-center">
         <Header title="Bridging The Canyon" pages={pages} />
-
         <main className="p-2 mt-20">{children}</main>
-        <Analytics />
         <Footer/>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
